2302445ace4d760a995bfa643ec035cc9e46db8e
DanielCripps94 <dan-cripps@hotmail.co.uk>
2020-04-22 15:42:36 +0100

    product image - bootstrap media introduced

--- checkout-page/src/components/ItemDetails.js
+++ checkout-page/src/components/ItemDetails.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Button, Collapse, Well, Media, Row, Col } from 'react-bootstrap'
-import { render } from '@testing-library/react';
+
 
 export default class ItemDetails extends Component {
     state = {
@@ -10,15 +10,29 @@ export default class ItemDetails extends Component {
             return (
                 <div>
                     <Button 
-                    className='item--details--button'
-                    bsStyle="link"
-                    onClick={() => this.setState({open: !this.state.open})}
+                        className="item--details--button"
+                        bsStyle="link"
+                        onClick={() => this.setState({open: !this.state.open})}
                     >
-                    { this.state.open === false? `See` : `Hide` } item details 
-                    { this.state.open === false? ` +` : ` -` }
-
+                        { this.state.open === false? `See` : `Hide` } item details 
+                        { this.state.open === false? ` +` : ` -` }
                     </Button>
+                    <hr/>
+                    <Collapse in={this.state.open}>
+                        <div>                           
+                            <Media>
+                                <Media.Body>
+                                    <img 
+                                    width={100}
+                                    height={100}
+                                    alt="thumbnail"
+                                    src="https://images-na.ssl-images-amazon.com/images/I/71MGjLfKikL._AC_SX679_.jpg"
+                                    />
+                                </Media.Body>
+                            </Media>                           
+                        </div>
+                    </Collapse>
                 </div>
-        )
+            )
+        }
     }
-}
